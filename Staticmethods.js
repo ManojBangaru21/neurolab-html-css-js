4a8e3bada2456b39e870b336899d306b24d91e74
ManojBangaru21 <bmanoj.ba@gmail.com>
2023-04-16 20:01:24 +0530

    Test2#

--- Staticmethods.js
+++ Staticmethods.js
@@ -2,7 +2,9 @@ class Person{
     constructor(name){
         this.name=name
     }
-
+    getName(){
+        return this.name
+    }
     static Create(gender){
         let name = gender == "Male" ? "Manoj" : "Likitha"
         return name
@@ -12,3 +14,6 @@ class Person{
 
 let name_gender = Person.Create("Female")
 console.log(name_gender)
+let name1 = new Person("Manoj")
+//name1.getName()
+console.log(name1.getName())
